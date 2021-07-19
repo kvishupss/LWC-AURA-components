5d1e501432f047bfa205b803263eae38e3197515
Vishvamber Singh Solanki <74855764+kvishupss@users.noreply.github.com>
2021-07-19 13:18:47 +0100

    Delete HouseholdAccountRelationController.js

--- HouseholdAccountRelationController.js
+++
@@ -1,8 +0,0 @@
-({
-    doInit : function(component, event, helper) {
-        helper.helperMethod(component);
-    },
-    handleSelect: function (component, event, helper) {  
-        helper.helperhandleSelect(component, event, helper);      
-    }
-})
