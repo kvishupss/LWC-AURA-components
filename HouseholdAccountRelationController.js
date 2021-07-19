17267d7ca20b88f5653016d832e144a5ee82b3b2
Vishvamber Singh Solanki <74855764+kvishupss@users.noreply.github.com>
2021-07-19 17:40:10 +0530

    Add files via upload

---
+++ HouseholdAccountRelationController.js
@@ -0,0 +1,8 @@
+({
+    doInit : function(component, event, helper) {
+        helper.helperMethod(component);
+    },
+    handleSelect: function (component, event, helper) {  
+        helper.helperhandleSelect(component, event, helper);      
+    }
+})
